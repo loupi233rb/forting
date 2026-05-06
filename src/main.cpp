424a45bac82187a18526812748cdd27a3123aaa0
loupi233 <yaoguang.lei@outlook.com>
2026-05-06 16:52:00 +0800

    路径适配

--- src/main.cpp
+++ src/main.cpp
@@ -22,9 +22,10 @@ int main(int argc, char *argv[])
     // printActions(parser.run(file.getFileList())[1]);
 
     fs::path p ="e:/gewu/";
-    string s = "/name/";
-    string s_ = "/size/";
+    string s = "name/";
+    string s_ = "size/";
     p.concat(s);
+    p.concat(s_);
     p.lexically_normal();
     p.make_preferred();
     cout<<p;
